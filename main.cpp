251ebca6d393f2a4d901fc74642156980ae0c2c0
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-22 15:14:45 +0530

    splitted into files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
+#include "./validity.cpp"
 
-using namespace std;
-int main()
-{
-    cout << "Hello, World!" << endl;
+int main() {
+    string query;
+    cout << "Enter a query: ";
+    getline(cin, query); // Use getline to read the entire line
+
+    if (validate(query)) {
+        cout << "Valid query" << endl;
+    } else {
+        cout << "Invalid query" << endl;
+    }
+
     return 0;
 }
